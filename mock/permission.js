75c6cb9fe67f7e3026bea56a42011d8a328fa205
iimeepo <2264535743@qq.com>
2020-08-05 09:11:11 +0800

    新增顶部菜单及左侧双菜单

--- mock/permission.js
+++ mock/permission.js
@@ -77,7 +77,7 @@ const routes = [
   {
     path: '/nested',
     component: 'Layout',
-    redirect: '/nested/menu1',
+    redirect: '/nested/menu2',
     name: 'Nested',
     meta: {
       title: '多级菜单',
