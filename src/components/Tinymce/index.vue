f3e35f0516f1117203f825d9e145211738039215
iimeepo <2264535743@qq.com>
2024-09-03 16:47:47 +0800

    更新package 更新tinymce编辑器cdn地址

--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -18,7 +18,7 @@ import toolbar from './toolbar'
 import load from './dynamicLoadScript'
 
 // why use this cdn, detail see https://github.com/PanJiaChen/tinymce-all-in-one
-const tinymceCDN = 'https://cdn.jsdelivr.net/npm/tinymce-all-in-one@4.9.3/tinymce.min.js'
+const tinymceCDN = 'https://fastly.jsdelivr.net/npm/tinymce-all-in-one@4.9.3/tinymce.min.js'
 
 export default {
   name: 'Tinymce',
